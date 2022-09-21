e1155e1ce820c133359ef5ac2e39ed9b5c4b9ce5
VictorEijkhout <eijkhout@tacc.utexas.edu>
2022-09-20 23:18:39 -0500

    adding more solutions

--- sessions/objects/pointclass.cxx
+++ sessions/objects/pointclass.cxx
@@ -25,6 +25,11 @@ public:
   float distance_to_origin() {
     return sqrt( x*x + y*y );
   }
+  auto dx(Point other) { return x-other.x; };
+  auto dy(Point other) { return y-other.y; };
+  // accessors
+  auto getx() { return x;};
+  auto gety() { return y;};
   float angle() {
     return atan(y/x);
   }
@@ -44,33 +49,61 @@ public:
 class LinearFunction {
 private:
   // data members
-  Point one,two;
-  // float slope,intercept;
-  // Point reference; float slope;
+  Point reference;
+  float slope;
 public:
-  // available function methods
-  // not the most elegant: LinearFunction( Point p,Point q ) { one=p; two=q; };
-  // better:
   LinearFunction( Point p,Point q ) 
-    : one(p),two(q) {};
+    : reference(p){
+    slope = p.dy(q) / p.dx(q);
+      // (q.y-p.y)/(q.x-p.x);
+  };
   float evaluate_at( float x ) {
-    // option1 : lots of thinking
-    // option2 : return x*slope+intercept
-    // option3 : return reference.y + (x-reference.x)*slope
+    float dx = x-reference.getx();
+    float dy = dx*slope;
+    return reference.gety()+dy;
   };
 };
 
+class Rectangle {
+protected:
+  Point bl;
+  float width,height;
+public:
+  Rectangle( Point p,float w,float h ) 
+    : bl(p),width(w),height(h) {};
+  float area() {
+    return width*height;
+  };
+  float circumference() {
+    return 2*(width+height);
+  };
+};
+
+class Square : public Rectangle {
+private:
+  // no data members specific to the square
+public:
+  Square( Point p,float side ) 
+    : Rectangle(p,side,side) {};
+};
+
 int main() {
-  Point p(1.0,1.0), q(2.0,0.5);
+  Point p(1.0,1.0), q(2.0,3.);
 
-  LinearFunction slope(p,q);
+  Rectangle pq_rect(p,2,1);
+  cout << "area: " << pq_rect.area() << '\n';
 
-  cout << "dist: " << p.distance(q) << '\n';
+  Square p_square(p,1.414);
+  cout << "area: " << p_square.area() << '\n';
 
-  float d = p.distance_to_origin();
-  cout << "Distance to origin: "
-       << d << endl;
-  cout << "angle: " << p.angle() << '\n';
+  // LinearFunction slope(p,q);
+
+  // cout << "dist: " << p.distance(q) << '\n';
+
+  // float d = p.distance_to_origin();
+  // cout << "Distance to origin: "
+  //      << d << endl;
+  // cout << "angle: " << p.angle() << '\n';
   
   return 0;
 }
